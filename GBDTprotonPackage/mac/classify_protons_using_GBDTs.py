978d2fffb88b9e9a5f0110a4eff546c809c98d65
Erez Cohen <cohen.erez7@gmail.com>
2017-01-06 12:01:13 +0200

    Fri Jan  6 12:01:13 IST 2017

--- GBDTprotonPackage/mac/classify_protons_using_GBDTs.py
+++ GBDTprotonPackage/mac/classify_protons_using_GBDTs.py
@@ -1,3 +1,4 @@
+
 from gbdt_tools import *
 '''
     usage:
@@ -13,13 +14,28 @@ from gbdt_tools import *
 '''
 
 
-TrainingDataType        = "MC_BNB"
-NumberOfTrainingEvents  = 300000
+TrainingDataType        = 'MC_BNB'                              # options: 'openCOSMIC_MC' , 'MC_BNB'
+NumberOfTrainingEvents  = 300000                                # 'openCOSMIC_MC': 200000 , 'MC_BNB': 300000
 TracksListName          = "BNB_5e19POT"
 GBDTmodelName           = "multi_BNB_TrainedOn_MCBNB_MCCOSMIC"  # options: 'BNB_TrainedOn_only_MC_BNB'
 score                   = 0.9                                   # p_score = 0.0 for cosmics
 maxscore                = 'muons'
 
+parameters = {  'debug':flags.verbose,
+                'objective':'binary:logistic' ,
+                'eta':0.025 ,
+                'eval_metric':'error',
+                'silent':1,
+                'nthread':6,
+                'min_child_weight':4,
+                'max_depth':13,
+                'gamma':0.7,
+                'colsample_bytree':0.5,
+                'subsample':0.8,
+                'Ntrees':500,
+                'Nfolds':10,
+#                'reg_alpha':1e-5
+                }
 
 
 # -------------------------------------------------------------------
@@ -31,7 +47,8 @@ if flags.option=="divide training and testing samples" or 'divide' in flags.opti
 # -------------------------------------------------------------------
 if flags.option=="train GBDTs cross validation" or 'train' in flags.option:
     
-    train_gbdt_cross_validation( TrainingDataType , NumberOfTrainingEvents )
+    #    train_gbdt_cross_validation( TrainingDataType , NumberOfTrainingEvents )
+    train_gbdt_MCBNB_and_CORSIKA( ['MC_BNB','openCOSMIC_MC'] , [300000,200000] )
 
 
 # -------------------------------------------------------------------
