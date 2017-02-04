50e6926b0ae44e68e6f93b49eec159f5759f8223
Erez Cohen <erez@helion.tau.ac.il>
2017-02-04 13:32:45 +0100

    sdf

--- GBDTprotonPackage/GBDTprotons_GBDTprotonPackageDict.cxx
+++ GBDTprotonPackage/GBDTprotons_GBDTprotonPackageDict.cxx
@@ -56,7 +56,7 @@ namespace ROOT {
       static ::TVirtualIsAProxy* isa_proxy = new ::TIsAProxy(typeid(::GBDTanalysis));
       static ::ROOT::TGenericClassInfo 
          instance("GBDTanalysis", "GBDTanalysis.h", 25,
-                  typeid(::GBDTanalysis), ::ROOT::Internal::DefineBehavior(ptr, ptr),
+                  typeid(::GBDTanalysis), DefineBehavior(ptr, ptr),
                   &GBDTanalysis_Dictionary, isa_proxy, 4,
                   sizeof(::GBDTanalysis) );
       instance.SetDelete(&delete_GBDTanalysis);
@@ -104,16 +104,16 @@ namespace {
 0
     };
     static const char* includePaths[] = {
-"/Users/erezcohen/larlite/UserDev/mySoftware",
-"/Users/erezcohen/larlite/UserDev/MyLarLite/MyPackage",
-"/Users/erezcohen/larlite/UserDev/AnalysisTreesInformation/AnaTreesPackage",
-"/Users/erezcohen/larlite/core",
-"/usr/local/Cellar/root6/6.06.02/include/root",
-"/Users/erezcohen/larlite/UserDev/GBDTprotons/GBDTprotonPackage/",
+"/home/erez/larlite/UserDev/mySoftware",
+"/home/erez/larlite/UserDev/MyLarLite/MyPackage",
+"/home/erez/larlite/UserDev/AnalysisTreesInformation/AnaTreesPackage",
+"/home/erez/larlite/core",
+"/usr/local/root/include/root",
+"/home/erez/larlite/UserDev/GBDTprotons/GBDTprotonPackage/",
 0
     };
-    static const char* fwdDeclCode = R"DICTFWDDCLS(
-#line 1 "libGBDTprotons_GBDTprotonPackage dictionary forward declarations' payload"
+    static const char* fwdDeclCode = 
+R"DICTFWDDCLS(
 #pragma clang diagnostic ignored "-Wkeyword-compat"
 #pragma clang diagnostic ignored "-Wignored-attributes"
 #pragma clang diagnostic ignored "-Wreturn-type-c-linkage"
@@ -121,7 +121,6 @@ extern int __Cling_Autoloading_Map;
 class __attribute__((annotate("$clingAutoload$GBDTanalysis.h")))  GBDTanalysis;
 )DICTFWDDCLS";
     static const char* payloadCode = R"DICTPAYLOAD(
-#line 1 "libGBDTprotons_GBDTprotonPackage dictionary payload"
 
 #ifndef G__VECTOR_HAS_CLASS_ITERATOR
   #define G__VECTOR_HAS_CLASS_ITERATOR 1
